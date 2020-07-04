58a156adc2ca25aaddb03814e51e906ffc9cf981
Carlo Del Don <carlo.deldon@gmail.com>
2020-07-04 16:15:06 +0100

    New way to deal with requirements and setup tox action

--- setup.py
+++ setup.py
@@ -10,23 +10,18 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
 
 setup(
     author="Carlo Del Don",
     author_email='carlo.deldon@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -44,9 +39,7 @@ setup(
     keywords='cartesio',
     name='cartesio',
     packages=find_packages(include=['cartesio', 'cartesio.*']),
-    setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/cdeldon/cartesio',
     version='0.1.0',
     zip_safe=False,
