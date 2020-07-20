d0eeb98fee838de4f82995451be243acb753830a
Carlo Del Don <cdeldon@users.noreply.github.com>
2020-07-20 09:44:56 +0100

    wip (#10)

--- cartesio/core/__init__.py
+++ cartesio/core/__init__.py
@@ -1,12 +1,5 @@
-import numpy as np
+from .numba_decorators import jit
 
 __all__ = [
-    "segment_length"
+    "jit"
 ]
-
-
-def segment_length(
-        segment: np.ndarray
-) -> float:
-    x1, y1, x2, y2 = segment
-    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
