2320147ae108c20ec2b69b5b5502665486b1797a
Carlo Del Don <carlo.deldon@gmail.com>
2020-09-01 12:30:14 +0100

    Bump version: 0.1.1 → 0.2.0

--- cartesio/__init__.py
+++ cartesio/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Carlo Del Don"""
 __email__ = "carlo.deldon@gmail.com"
-__version__ = "__version__ = '0.1.1'"
+__version__ = "__version__ = '0.2.0'"
 
 __all__ = ["core", "bbox"]
 
